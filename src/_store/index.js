a3051a067e2ba382ed5713a353d236dfa5b9f4cf
msciborski <michal.scibrski@student.put.poznan.pl>
2019-05-21 23:01:26 +0200

    dispatch getUserAction

--- src/_store/index.js
+++ src/_store/index.js
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import fb from '../config';
 
 import rootReducer from '../_reducers';
 
-
+const loggerMiddleware = createLogger();
 const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    applyMiddleware(
+        thunk.withExtraArgument({ getFirebase, getFirestore }),
+        loggerMiddleware,
+    ),
     reactReduxFirebase(fb, { userProfile: 'users', useFirestoreForProfile: true, attachAuthIsReady: false }),
     reduxFirestore(fb),
 ));
