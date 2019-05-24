dca4fe21fbbd2fdce52d45e1d71e0573025556dd
msciborski <michal.scibrski@student.put.poznan.pl>
2019-05-25 00:08:27 +0200

    config auth firebase

--- android/app/src/main/java/com/shoplist/MainApplication.java
+++ android/app/src/main/java/com/shoplist/MainApplication.java
@@ -3,8 +3,11 @@ package com.shoplist;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new RNFirebaseFirestorePackage(), 
+            new RNFirebaseFirestorePackage(),
+            new RNFirebaseAuthPackage(), 
             new RNGestureHandlerPackage()
       );
     }
