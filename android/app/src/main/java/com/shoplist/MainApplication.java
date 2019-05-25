647b21b8f739d5724e2ee8cf46ea8567ad2796ab
msciborski <michal.scibrski@student.put.poznan.pl>
2019-05-25 13:57:43 +0200

    add react-native-elements

--- android/app/src/main/java/com/shoplist/MainApplication.java
+++ android/app/src/main/java/com/shoplist/MainApplication.java
@@ -3,6 +3,7 @@ package com.shoplist;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseFirestorePackage(),
             new RNFirebaseAuthPackage(), 
