f1a8e7006493b97e58e4c4c1ea5a3225f1ba4738
msciborski <michal.scibrski@student.put.poznan.pl>
2019-05-26 18:43:56 +0200

    Singup screen

--- src/screens/SingupScreen/SingupScreen.js
+++ src/screens/SingupScreen/SingupScreen.js
@@ -1,23 +1,94 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Input, Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { usersAuthActions } from '../../_actions';
+import { HomeScreen } from '../HomeScreen';
+
 
 const styles = StyleSheet.create({
-    cardBody: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    input: {
-        width: '100%',
+    card: {
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
     }
-
 })
 
-export class SingUpScreen extends Component {
+class SingUpScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    handleSingUp = () => {
+        const { singUp } = this.props;
+        const { email, password } = this.state;
+
+        singUp(email, password);
+        this.setState({ email: '', password: '' })
+    }
+
     render() {
+        const { email, password } = this.state;
+
         return (
-            <Text>Singup Screen</Text>
+            <View style={{flexDirection: 'row'}}>
+            <Card>
+                <View>
+                    <Input
+                        placeholder='Email'
+                        leftIcon={
+                            <Icon
+                                name='envelope'
+                                size={18}
+                                color='black'
+                            />
+                        }
+                        onChangeText={text => this.setState({ email: text })}
+                        value={email}
+                        style={{width: 200}}
+                    />
+                    <Input
+                        placeholder='Password'
+                        leftIcon={
+                            <Icon
+                                name='key'
+                                size={18}
+                                color='black'
+                            />
+                        }
+                        onChangeText={text => this.setState({ password: text })}
+                        value={password}
+                    />
+                    <Button title="Sing up" onPress={this.handleSingUp}/>
+                </View>
+            </Card>
+            </View>
         );
     }
 }
+
+const mapStateToProps = state => {
+    const { authUser } = state;
+    
+    return {
+        authUser,
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        singUp: (email, password) => dispatch(usersAuthActions.singUp(email, password)),     
+    }
+}
+
+const connectedSingUpScreen = connect(mapStateToProps, mapDispatchToProps)(SingUpScreen);
+
+export { connectedSingUpScreen as SingUpScreen };
