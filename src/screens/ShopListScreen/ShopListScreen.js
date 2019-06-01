4c35b69bf85d0d91638ababb997329d46bd3a4e7
msciborski <michal.sciborski@student.put.poznan.pl>
2019-06-01 15:29:04 +0200

    Stack actions in ShopListScreen

--- src/screens/ShopListScreen/ShopListScreen.js
+++ src/screens/ShopListScreen/ShopListScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { ListItem } from 'react-native-elements';
 
@@ -16,15 +16,58 @@ export class ShopListScreen extends Component {
                     onPress={() => navigation.navigate('App')}
                 />
             ),
+            headerRight: (
+                !navigation.getParam('addButtonPressed') ?
+                    <Icon
+                        name="plus"
+                        size={30}
+                        style={{ padding: 10 }}
+                        onPress={() => navigation.getParam('handleAddButton')()}
+                    /> :
+                    <View style={{ flexDirection: 'row'}}>
+                        <Icon 
+                            name="check"
+                            size={30}
+                            style={{ padding: 10 }}
+                            onPress={() => navigation.getParam('handleAcceptButton')()}
+                        />
+                        <Icon 
+                            name="times"
+                            size={30}
+                            style={{ padding: 10 }}
+                            onPress={() => navigation.getParam('handleCancelButton')()}
+                        />
+                    </View>
+            )
         };
     }
 
     constructor(props) {
         super(props);
+        this.props.navigation.setParams({ handleAddButton: this.handleAddButtonPress, handleCancelButton: this.handleCancelButtonPress, handleAcceptButton: this.handleAcceptButtonPress });
+        this.state = {
+            addButtonPressed: false,
+        }
+    }
+
+    handleAddButtonPress = () => {
+        this.props.navigation.setParams({ addButtonPressed: true });
+        this.setState({ addButtonPressed: true });
+    }
+
+    handleCancelButtonPress = () => {
+        this.props.navigation.setParams({ addButtonPressed: false });
+        this.setState({ addButtonPressed: false });
+    }
+
+    handleAcceptButtonPress = () => {
+        this.props.navigation.setParams({ addButtonPressed: false });
+        this.setState({ addButtonPressed: false });
     }
 
     render() {
         const { navigation } = this.props;
+        const { addButtonPressed } = this.state;
         const shopListElements = navigation.getParam('shopListElements');
 
         return (
@@ -38,6 +81,10 @@ export class ShopListScreen extends Component {
                             />
                         )
                     })
+                }
+                {
+                    addButtonPressed &&
+                        <Text>Button clicked</Text>
                 }
             </View>
         )
